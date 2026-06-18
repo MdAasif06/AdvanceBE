a5cded36a3748ef2caf5b2066e2f5a4462c92f15
MdAasif06 <abdurm477@gmail.com>
2026-06-18 15:55:32 +0530

    advance backend very essential for better understanding

---
+++ level1/phase1/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ messsage: "Hello from docker" });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is running port at ${port}`);
+});
