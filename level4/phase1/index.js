512253c5e448aa69913ed573cce55470fd542c34
MdAasif06 <abdurm477@gmail.com>
2026-06-24 15:29:39 +0530

    this system store previous data and response correctly

--- level4/phase1/index.js
+++ level4/phase1/index.js
@@ -7,6 +7,7 @@ import {
   Annotation,
   MessagesAnnotation,
   StateGraph,
+  MemorySaver
 } from "@langchain/langgraph";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { TavilySearch } from "@langchain/tavily";
@@ -18,12 +19,11 @@ app.use(express.json());
 const tool = new TavilySearch({
   maxResults: 2,
   topic: "general",
- 
 });
+const checkPointer = new MemorySaver()
 
 const tools = [tool];
 const toolNode = new ToolNode(tools);
-
 
 ///here created model
 const model = new ChatGroq({
@@ -38,29 +38,35 @@ const State = Annotation.Root({
   aiMsg: Annotation,
 });
 
-
-
-
 const callLLM = async (state) => {
   console.log("state", state);
   const response = await model.invoke([
     {
       role: "system",
-      content: `You are a assistent and your name is jarvis.
-      If you don't know the answer then call relevent tool`,
+      content: `You are Jarvis AI assistant
+
+Use conversation memory first.
+
+Only use tools when the answer requires
+external real-time information like:
+weather, news, web search, stock prices etc.
+
+Do NOT call tools for simple conversation,
+memory-based questions, greetings,
+or personal context`,
     },
     // { role: "human", content: state.messages[0].content },
-    ...state.messages
+    ...state.messages,
   ]);
   return { messages: [response] };
 };
 
 const shouldContinue = async (state) => {
-  const lastMessage=state.messages[state.messages.length-1]
-  if(lastMessage.tool_calls.length>0){
-    return "tools"
-  }else{
-     return "__end__"
+  const lastMessage = state.messages[state.messages.length - 1];
+  if (lastMessage.tool_calls.length > 0) {
+    return "tools";
+  } else {
+    return "__end__";
   }
 };
 
@@ -68,19 +74,29 @@ const graph = new StateGraph(MessagesAnnotation)
   .addNode("agent", callLLM)
   .addNode("tools", toolNode)
   .addEdge("__start__", "agent")
-  .addEdge("tools","agent")
-  .addConditionalEdges("agent",shouldContinue)
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue)
   // .addEdge("agent", "__end__")
-  .compile();
+  .compile({ checkpointer: checkPointer });
 
 app.post("/ai", async (req, res) => {
   const { input } = req.body;
-  const response = await graph.invoke({
-    messages: [{ role: "user", content: input }],
-  });
+  const response = await graph.invoke(
+    {
+      messages: [
+        {
+          role: "user",
+          content: input,
+        },
+      ],
+    },
+    { configurable: { thread_id: "user123" } },
+  );
   console.log(response);
 
-  return res.status(200).json({ ai: response.messages[response.messages.length-1].content });
+  return res
+    .status(200)
+    .json({ ai: response.messages[response.messages.length - 1].content });
 });
 
 app.get("/", (req, res) => {
