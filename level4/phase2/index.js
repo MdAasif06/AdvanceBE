08ea9a48728117b7b439b133d5bb8485ad703afa
MdAasif06 <abdurm477@gmail.com>
2026-06-25 14:42:09 +0530

    creating verctor db using quadrant

--- level4/phase2/index.js
+++ level4/phase2/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-// import {} from "@langchain/core";
+import {} from "@langchain/core";
 import { ChatGroq } from "@langchain/groq";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { PDFParse } from "pdf-parse";
+import fs from "fs";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +22,35 @@ const llm = new ChatGroq({
   maxTokens: 50,
   maxRetries: 2,
 });
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  model: "gemini-embedding-001", // 768 dimensions
+  taskType: TaskType.RETRIEVAL_DOCUMENT,
+  title: "Document title",
+});
+
+
+const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  url: process.env.QDRANT_URL,
+  collectionName: "grocery-store",
+});
+
+const upload = async () => {
+  const pdfPath = "./knowledge.pdf";
+  const buffer = fs.readFileSync(pdfPath);
+  const pdfResult = new PDFParse({ data: buffer });
+  const result = await pdfResult.getText();
+  const text = result.text;
+  const spiliter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+  const docs = await spiliter.createDocuments([text]);
+  // console.log(docs);
+  await vectorStore.addDocuments(docs)
+};
+upload();
+
 
 app.post("/ai", async (req, res) => {
   const { input } = req.body;
